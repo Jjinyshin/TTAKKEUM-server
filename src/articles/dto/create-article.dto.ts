c4c4ba050429d7a30277ad362eab581383c360d1
Jinyshin <iwbm312@gmail.com>
2024-07-13 22:11:55 +0900

    feat: add validation to articleDto

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,12 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   @ApiProperty({ description: '게시글 제목' })
   title: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(500)
   @ApiProperty({ description: '게시글 내용' })
   content: string;
 
+  @IsString()
+  @IsOptional()
   @ApiProperty({
     required: false,
     nullable: true,
@@ -14,12 +31,15 @@ export class CreateArticleDto {
   })
   imageUrl?: string;
 
+  @IsArray()
+  @ArrayMaxSize(5)
+  @IsOptional()
   @ApiProperty({
     required: false,
     nullable: true,
     description: '해시태그',
   })
-  hashtag?: any;
+  hashtag?: string[];
 
   // TODO: change to Token
   @ApiProperty({ description: '작성자 ID' })
