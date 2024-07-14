33012cf9e2b449ce193dd99fe2651f9f7abba4d0
Jinyshin <iwbm312@gmail.com>
2024-07-14 16:20:35 +0900

    feat: add validation to userentity

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ description: '사용자 이메일' })
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ description: '사용자 닉네임' })
   nickname: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ description: '도치 이름' })
   dochiname: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   @ApiProperty({ description: '사용자 비밀번호' })
   password: string;
 }
