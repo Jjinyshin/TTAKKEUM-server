fbfebdc0240aac7543f457327d264c3924cef701
Jinyshin <iwbm312@gmail.com>
2024-07-15 02:48:00 +0900

    feat: add jwt auth guard

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,6 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   controllers: [UsersController],
   providers: [UsersService],
   imports: [PrismaModule],
-  exports: [UsersModule],
+  exports: [UsersService],
 })
 export class UsersModule {}
