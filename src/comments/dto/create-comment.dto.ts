8ac0e1bb7ea3a224758e7248346bde8f65339ca6
Jinyshin <iwbm312@gmail.com>
2024-07-15 21:15:43 +0900

    feat: define the Comment entity and DTO classes

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,18 @@
-export class CreateCommentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+export class CreateCommentDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  content: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  articleId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty()
+  userId: number;
+}
--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,22 @@
-export class Comment {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Comment } from '@prisma/client';
+
+export class CommentEntity implements Comment {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  articleId: number;
+
+  @ApiProperty()
+  userId: number;
+}
