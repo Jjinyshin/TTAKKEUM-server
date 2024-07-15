909c86938823ada0f6989608ccc31cf8cc9d8ab1
Jinyshin <iwbm312@gmail.com>
2024-07-15 21:09:01 +0900

    feat: add PrismaClient to the Comments module

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CommentsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
