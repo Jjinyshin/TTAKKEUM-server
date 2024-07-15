c2ba4363a9b80c23fa1d667ce2e36567e9c1457c
Jinyshin <iwbm312@gmail.com>
2024-07-15 14:14:38 +0900

    feat: add custom decorators to save authorid from Bearer token

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -9,11 +9,13 @@ import {
   ParseIntPipe,
   UseInterceptors,
   UploadedFile,
+  UseGuards,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import {
+  ApiBearerAuth,
   ApiConsumes,
   ApiCreatedResponse,
   ApiOkResponse,
@@ -22,6 +24,9 @@ import {
 import { ArticleEntity } from './entities/article.entity';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CreateArticleRequestDto } from './dto/create-article-req.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('articles')
 @ApiTags('articles')
@@ -29,12 +34,15 @@ export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiConsumes('multipart/form-data')
   @UseInterceptors(FileInterceptor('image'))
   @ApiCreatedResponse({ type: ArticleEntity })
   async create(
     @Body() body: CreateArticleRequestDto,
     @UploadedFile() image: Express.Multer.File,
+    @CurrentUser() user: User,
   ): Promise<ArticleEntity> {
     // 해시태그 변환 로직 추가
     let hashtags: string[] = [];
@@ -48,7 +56,7 @@ export class ArticlesController {
     createArticleDto.content = body.content;
     createArticleDto.image = body.image;
     createArticleDto.hashtag = hashtags;
-    createArticleDto.authorId = body.authorId;
+    createArticleDto.authorId = user.id;
 
     return new ArticleEntity(
       await this.articlesService.create(createArticleDto, image),
