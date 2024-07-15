a6f32059cffcf50f5087d2cb2368ad20e19bb3ed
Jinyshin <iwbm312@gmail.com>
2024-07-15 23:48:24 +0900

    fix: comment constructor to except null user

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,7 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Comment } from '@prisma/client';
+import { ArticleEntity } from 'src/articles/entities/article.entity';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 export class CommentEntity implements Comment {
+  constructor(data: Partial<CommentEntity>) {
+    Object.assign(this, data);
+    if (data.user) {
+      this.user = new UserEntity(data.user);
+    }
+    if (data.article) {
+      this.article = new ArticleEntity(data.article);
+    }
+  }
+
   @ApiProperty()
   id: number;
 
@@ -19,4 +31,10 @@ export class CommentEntity implements Comment {
 
   @ApiProperty()
   userId: number;
+
+  @ApiProperty({ type: () => ArticleEntity })
+  article: ArticleEntity;
+
+  @ApiProperty({ type: () => UserEntity })
+  user: UserEntity;
 }
